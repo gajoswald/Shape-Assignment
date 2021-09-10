d80ed26f9f608c81f85ac83a7c42d2f999ae8585
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2021-09-10 15:45:52 +0000

    New branch. Stripped out assignment code.

--- script.js
+++ script.js
@@ -20,37 +20,8 @@ function setup() {
 
 function draw() {
   myShape();
-  // myOutlineShape();
 }
 
 function myShape() {
   noStroke();
-  fill( "#003B70");
-  rect(100, 100, 200, 30);
-  rect(185,100,30,200);
-  // r1 = (215 - 100)/2 = 57.5
-  // x1 = 100 + r1 
-  arc(157.5,300,57.5,57.5,0,PI);
-  fill('white');
-  // r2 = r1 - 30 = 27.5
-  // x2 = 100 + 30 + r2
-  arc(157.5, 300,27.5, 27.5, 0, PI);
-  fill( "#003B70");
-  rect(100,280,30,20)
-  noLoop();
-}
-
-function myOutlineShape() {
-  stroke("#CF0A2C");
-  fill(random(colorlist));
-  rect(100, 100, 200, 30);
-  fill(random(colorlist));
-  rect(185,100,30,200);
-  fill(random(colorlist));
-  arc(157.5,300,57.5,57.5,0,PI);
-  fill('white');
-  arc(157.5, 300,27.5, 27.5, 0, PI);
-  fill(random(colorlist));
-  rect(100,280,30,20)
-  noLoop();
 }
